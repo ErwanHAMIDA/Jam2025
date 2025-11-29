8505b496aea4053f492c7052d4ccba139f0cfeb6
Alexandre Glatz <149509232+AlexandreGlatz@users.noreply.github.com>
2025-11-29 19:38:34 +0100

    [ADD] Shop

--- Assets/_script/Shop.cs
+++ Assets/_script/Shop.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public struct Item
 {
@@ -8,27 +10,23 @@ public struct Item
     public string description;
     public int price;
     public Sprite thumbnail;
+
+    public Item(string itemName, string itemDesc, int itemPrice, Sprite itemThumb)
+    {
+        name = itemName; description = itemDesc; price = itemPrice; thumbnail = itemThumb;
+    }
 }
 public class Shop : MonoBehaviour
 {
-    Dictionary<string, int> _buyableItems;
-    GameObject _buttonPrefab;
-
+    List<Item> _buyableItems = new List<Item>();
+    [SerializeField] GameObject _buttonPrefab;
+    [SerializeField] GameObject _prefabParent;
+    [SerializeField] List<Sprite> sprites;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _buyableItems = new Dictionary<string, int>() {
-            { "ice",        99999 },
-            { "mint",       99999 },
-            { "orange",     99999 },
-            { "lemon",      99999 },
-            { "pepper",     99999 },
-            { "gasoline",   99999 },
-            { "rum",        99999 },
-            { "beer",       99999 },
-            { "lemonade",   99999 },
-            { "tea",        99999 },
-        };
+        _buyableItems.Add(new Item("ice", "Get it fresh !", 10, sprites[0]));
+        _buyableItems.Add(new Item("mint", "breath freshener", 10, sprites[1]));
 
         CreateShop();
     }
@@ -40,13 +38,20 @@ public class Shop : MonoBehaviour
 
     void CreateShop()
     {
-        foreach (KeyValuePair<string, int> pair in _buyableItems)
+        foreach (var item in _buyableItems)
         {
-            
+            GameObject newButton = Instantiate(_buttonPrefab, _prefabParent.transform);
+            newButton.SetActive(true);
+            newButton.transform.Find("Name").GetComponent<TMP_Text>().text = item.name;
+            newButton.transform.Find("Description").GetComponent<TMP_Text>().text = item.description;
+            newButton.transform.Find("Price").GetComponent<TMP_Text>().text = item.price.ToString();
+            newButton.transform.Find("Thumbnail").GetComponent<Image>().sprite = item.thumbnail;
+
+            //newButton.GetComponent<Button>().onClick.AddListener(() => Buy(item.price));
         }
     }
-   public void Buy(string name)
+   public void Buy(int price)
    {
-
+        //GameData.Gold -= price;
    }
 }
