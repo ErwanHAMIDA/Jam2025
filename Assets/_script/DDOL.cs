33804dafac503b56738d079ecbe6a8b4ab0b08ea
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2025-11-30 14:02:41 +0100

    [OKAY]

--- Assets/_script/DDOL.cs
+++ Assets/_script/DDOL.cs
@@ -5,6 +5,10 @@ public class DDOL : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (this != null)
+        {
+            Destroy(this);
+        }
         DontDestroyOnLoad(this);
     }
 
