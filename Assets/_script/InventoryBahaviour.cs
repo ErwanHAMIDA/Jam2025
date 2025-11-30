cb2b52fd140bccb5ea56de6cf2ee56466fb54a08
Alexandre Glatz <aglatz@gaming.tech>
2025-11-30 11:06:18 +0100

    [ADD] SFX and Shaker visuals

--- Assets/_script/InventoryBahaviour.cs
+++ Assets/_script/InventoryBahaviour.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class InventoryBahaviour : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    [SerializeField] AudioClip _clip;
     public void Toggle()
     {
         gameObject.SetActive(!gameObject.activeSelf);
+        SFXManager.Instance.PlaySFXClip(_clip, transform, 1);
     }
 
     void Start()
