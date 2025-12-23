435e332f4ae09fd1c3e7f767c979319a4dcfbd2f
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2025-12-23 14:22:08 +0100

    [ADD] SetInventoryOnData by default

--- Assets/_script/Datas/GameData.cs
+++ Assets/_script/Datas/GameData.cs
@@ -1,4 +1,6 @@
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -8,6 +10,7 @@ public class GameData
     public static GameData Instance => _instance ??= new GameData();
     [JsonProperty] public int Gold { get; private set; }
     [JsonProperty] public string Name { get; private set; }
+    [JsonProperty] public Dictionary<Ingredient, int> Inventory { get; private set; }
 
     private UITextManager _uiTextManager;
     private string _inputBarName;
@@ -18,16 +21,30 @@ public class GameData
         _uiTextManager.UpdateUIText();
     }
 
+    public void AddItem(Ingredient ingredient, int number)
+    {
+        Inventory.Add(ingredient, number);
+    }
+
     public void CopyFrom(GameData loaded)
     {
         SetGold(loaded.Gold);
         SetName(loaded.Name);
     }
 
-    public void SetDataByDefault()
+    public void SetDataByDefault(int gold , Ingredient[] ingredients, int[] quantity)
     {
+        if (ingredients.Length != quantity.Length)
+            throw new ArgumentOutOfRangeException("ingredients and quantity not the same count", nameof(ingredients.Length) + nameof(quantity.Length));
+
+        for (int i = 0; i < ingredients.Length; i++)
+        {
+            AddItem(ingredients[i], quantity[i]);
+        }
+
         SetGold(1000);
         SetName(_inputBarName);
+
         _uiTextManager.UpdateUIText();
     }
 
