fad98aed6c1287938bea209b88170d1acaf72b9d
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2025-12-29 20:23:54 +0100

    [ADD] Scriptable object Ingredient

---
+++ Assets/_script/ScriptableObject/IngredientData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IngredientData", menuName = "Scriptable Objects/IngredientData")]
+public class IngredientData : ScriptableObject
+{
+    [Header("Identification")]
+    public string _ingredientId;   // Unique id if needed for save/lookup.
+    public string _ingredientName; // Display name.
+
+    [Tooltip("Sprite of the card front.")]
+    public Sprite _ingredientTexture;
+
+    [Header("Game info")]
+    public string _description; // Optional description or lore.
+}
