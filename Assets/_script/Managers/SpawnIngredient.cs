47b6c2d159788cddeb221b2435c1ef8a20af8249
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2025-12-29 17:08:01 +0100

    [FIX] SpawnPoint Ingredient

--- Assets/_script/Managers/SpawnIngredient.cs
+++ Assets/_script/Managers/SpawnIngredient.cs
@@ -4,14 +4,15 @@ public class SpawnIngredient : MonoBehaviour
 {
     [SerializeField] private Camera worldCamera;
     [SerializeField] private GameObject _spawnPoint;
+    [SerializeField] private Vector3 _spawnCoord;
     
     public void InstantiateIngredient(GameObject ingredient)
     {
-        Vector3 worldPos = worldCamera.ScreenToWorldPoint(_spawnPoint.transform.position);
+        //Vector3 worldPos = worldCamera.ScreenToWorldPoint(_spawnPoint.transform.position + _offset);
 
-        worldPos.z = 0f;
+        _spawnCoord.z = 0f;
 
-        GameObject newIngredient = Instantiate(ingredient, worldPos, Quaternion.identity);
+        GameObject newIngredient = Instantiate(ingredient, _spawnCoord, Quaternion.identity);
         newIngredient.tag = "Ingredient";
         newIngredient.AddComponent<IngredientInShaker>();
     }
